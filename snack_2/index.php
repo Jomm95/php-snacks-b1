be769a604805c938cdf24388c14c96761eafcef3
Thomas <venturini.thomas95@gmail.com>
2022-03-15 15:36:30 +0100

    .

--- snack_2/index.php
+++ snack_2/index.php
@@ -1,10 +1,29 @@
+<!-- Passare come parametri GET name, mail e age e
+verificare (cercando i metodi che non conosciamo nella documentazione)
+che name sia più lungo di 3 caratteri,
+che mail contenga un punto e una chiocciola
+e che age sia un numero. 
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
+
 <?php
 
+// imposto che i parametri del get vadano inseriti, altrimenti si richiederà di farlo
+if (!empty($_GET['name']) && !empty($_GET['email']) && !empty($_GET['age'])) {
 
+    $name = $_GET['name'];
+    $mail = $_GET['mail'];
+    $age = $_GET['age'];
 
+    if (strlen($name) >= 3 && strpos($mail, '@') !== false  && strpos($mail, '.') !== false && is_numeric($age)) {
+        echo 'La formattazione dei dati è corretta, procedi pure!';
+    } else {
+        echo 'Il permesso ti è negato, riprova ad inserire i dati!';
+    }
 
+}
 
-
+// ?name=thom&mail=vent@mail.com&age=32 stringa da inserire nell'URL di prova
 
 ?>
 
